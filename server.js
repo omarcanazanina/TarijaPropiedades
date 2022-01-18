dde06f3b3766de0839b598877be8250b757aafd3
dev <you@gmail.com>
2022-01-18 15:23:05 -0400

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 // load the things we need
 var express = require('express');
+const { models } = require('./libs/sequelize')
 var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
+app.use('/static', express.static(__dirname + '/public'));
 // use res.render to load up an ejs view file
 
 // index page 
-app.get('/', function(req, res) {
+app.get('/',async function(req, res) {
+     const data = await models.User.findAll()
+        console.log(data, 'data')
+
     var mascots = [
         { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
         { name: 'Tux', organization: "Linux", birth_year: 1996},
@@ -26,6 +30,6 @@ app.get('/', function(req, res) {
 app.get('/about', function(req, res) {
     res.render('pages/about');
 });
-
-app.listen(8080);
-console.log('8080 is the magic port');
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(port +' is the magic port');
